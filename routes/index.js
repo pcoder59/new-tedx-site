af9eb28372f89e305b455a93f5097acdfdcac34b
Prem Raj <premraj59128@protonmail.com>
2021-10-30 21:17:25 +0530

    Implement contact form

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/contact', function(req, res, next) {
+  var mailOptions = {
+    from: req.body.email,
+    to: "event.tedkochi@gmail.com",
+    subject: req.body.subject,
+    text: "Name: " + req.body.name + "\n\n" + req.body.message
+  }
+  transporter.sendMail(mailOptions, function(error, info) {
+    if(error) {
+      res.render('index', { senderror: true, default: true });
+    } else {
+      res.render('index', { success: true, default: true });
+    }
+  });
+});
+
 module.exports = router;
