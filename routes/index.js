77ee36f0216aa0d9ee02637be7eaff78ae8333a8
Prem Raj <premraj59128@protonmail.com>
2021-10-30 21:39:37 +0530

    validate

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var nodemailer = require('nodemailer')
+var nodemailer = require('nodemailer');
+const { check, validationResult } = require('express-validator');
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
@@ -14,21 +15,26 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/contact', function(req, res, next) {
-  console.log(req)
-  var mailOptions = {
-    from: req.body.email,
-    to: "event.tedkochi@gmail.com",
-    subject: req.body.subject,
-    text: "Name: " + req.body.name + "\n\n" + req.body.message
-  }
-  transporter.sendMail(mailOptions, function(error, info) {
-    if(error) {
-      res.render('index', { senderror: true, default: true });
-    } else {
-      res.render('index', { success: true, default: true });
+router.post('/contact', [check('name').isLength({min: 1}).withMessage("Name is Required"), check('email').isEmail().withMessage("Invalid Email"), check('message').isLength({min: 1}).withMessage("Message is Required")], function(req, res, next) {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    res.redirect('/', { error: errors.mapped()});
+  } else {
+    var mailOptions = {
+      from: req.body.email,
+      to: "event.tedkochi@gmail.com",
+      subject: req.body.subject,
+      text: "Name: " + req.body.name + "\n\n" + req.body.message
     }
-  });
+    transporter.sendMail(mailOptions, function(error, info) {
+      if(error) {
+        res.render('index', { senderror: true, default: true });
+      } else {
+        res.render('index', { success: true, default: true });
+      }
+    });
+  }
+  console.log(req)
 });
 
 module.exports = router;
