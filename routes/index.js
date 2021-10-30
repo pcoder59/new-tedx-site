33daabe26d8207171a9210cfa8d2f6c83d975d20
Prem Raj <premraj59128@protonmail.com>
2021-10-30 21:41:13 +0530

    fix

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,7 @@ router.get('/', function(req, res, next) {
 router.post('/contact', [check('name').isLength({min: 1}).withMessage("Name is Required"), check('email').isEmail().withMessage("Invalid Email"), check('message').isLength({min: 1}).withMessage("Message is Required")], function(req, res, next) {
   const errors = validationResult(req);
   if(!errors.isEmpty()) {
-    res.redirect('/', { error: errors.mapped()});
+    res.redirect('/');
   } else {
     var mailOptions = {
       from: req.body.email,
