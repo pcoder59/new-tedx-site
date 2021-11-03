48c95335fd617ad04ef0bcfea0d24316515f1892
Prem Raj <premraj59128@protonmail.com>
2021-11-03 21:27:27 +0530

    revert

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/contact', [check('name').isLength({min: 1}).withMessage("Name is Required"), check('email').isEmail().withMessage("Invalid Email"), check('message').isLength({min: 1}).withMessage("Message is Required")], function(req, res, next) {
+router.get('/contact', [check('name').isLength({min: 1}).withMessage("Name is Required"), check('email').isEmail().withMessage("Invalid Email"), check('message').isLength({min: 1}).withMessage("Message is Required")], function(req, res, next) {
   const errors = validationResult(req);
   if(!errors.isEmpty()) {
     res.redirect('/');
